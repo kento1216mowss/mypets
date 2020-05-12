ecda42b92596858201b0148cdee8776dd1f5fae4
EC2 Default User <ec2-user@ip-172-31-41-138.ap-northeast-1.compute.internal>
2020-05-12 23:59:19 +0000

    update3

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load', function() {
-  $('.tweet-item').jscroll({
+  $('.bottomm').jscroll({
     // 無限に追加する要素は、どこに入れる？
-    contentSelector: 'tweet-item', 
+    contentSelector: '.tweet-item', 
     // 次のページにいくためのリンクの場所は？ ＞aタグの指定
     nextSelector: 'a.next',
     // 読み込み中の表示はどうする？
