9fd73a04b4ac4fefcef57286bb7cc03733c73e27
kento fujimoto <kento1216mowss@gmail.com>
2020-05-17 06:47:19 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 
 Rails.application.routes.draw do
- devise_for :users
+ devise_for :users, controllers: {   registrations: 'users/registrations',
+                                    sessions: 'users/sessions' }
  root 'tweets#index'
  resources :tweets do
     resources :comments, only: [:create]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_121353) do
+ActiveRecord::Schema.define(version: 2020_05_17_043317) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -75,5 +75,6 @@ ActiveRecord::Schema.define(version: 2020_04_28_121353) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 end
