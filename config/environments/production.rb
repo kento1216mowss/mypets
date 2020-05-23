a10563a6de501e0f45dd89fc76d8f87f93bd4b38
kento fujimoto <kento1216mowss@gmail.com>
2020-05-23 19:37:39 +0000

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,10 +100,10 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :user_name => ENV['SENDGRID_ADRESS'], 
-      :password =>  ENV['SENDGRID_PASSWORD'], 
-      :domain => "heroku.com",
-      :address => "smtp.sendgrid.net",
+      :user_name => ENV['MAIL_ADRESS'], 
+      :password =>  ENV['APP_PASSWORD'], 
+      :domain => "smtp.gmail.com",
+      :address => "smtp.gmail.com",
       :port => 587,
       :authentication => :plain,
       :enable_starttls_auto => true
