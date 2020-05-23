17cb27c30968b1620c3abb90fd01eded28f42645
kento fujimoto <kento1216mowss@gmail.com>
2020-05-23 19:06:30 +0000

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,16 +96,16 @@ Rails.application.configure do
   
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { protocol: 'https', host: 'https://rocky-garden-66161.herokuapp.com/' }
-  config.action_mailer.raise_delivery_errors = false　
+  config.action_mailer.default_url_options = {host: 'https://rocky-garden-66161.herokuapp.com/'}
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address => "smtp.sendgrid.net",
-    :port => 587,
-    :domain => 'heroku.com',
-    :user_name => ENV['SENDGRID_ADRESS'], #先ほど作成した送信用gmailアドレス
-    :password => ENV['SENDGRID_PASSWORD'], #認証したアカウントで発行したアプリパスワード
-    :authentication => 'login'
+      :user_name => ENV['SENDGRID_ADRESS'], 
+      :password =>  ENV['SENDGRID_PASSWORD'], 
+      :domain => "heroku.com",
+      :address => "smtp.sendgrid.net",
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true
   }
 end
