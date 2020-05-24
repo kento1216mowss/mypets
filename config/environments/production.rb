5de954cf00bf4ba7a4274e157e27e64eda483771
kento fujimoto <kento1216mowss@gmail.com>
2020-05-24 11:23:57 +0000

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,11 +102,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       :user_name => ENV['MAIL_ADRESS'], 
       :password =>  ENV['APP_PASSWORD'], 
-      
       :domain => "smtp.gmail.com",
       :address => "smtp.gmail.com",
       :port => 587,
-      :authentication => 'login',
+      :authentication => :plain,
       :enable_starttls_auto => true
   }
 end
