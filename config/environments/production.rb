eb6561b867feb2fc81193338bb200fa160315cfb
kento fujimoto <kento1216mowss@gmail.com>
2020-05-24 10:56:48 +0000

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.default_url_options = {host: 'https://rocky-garden-66161.herokuapp.com/'}
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :user_name => ENV['MAIL_ADRESS'], 
