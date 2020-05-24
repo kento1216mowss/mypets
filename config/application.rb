bc523564a949ed355e9ad213f5ff6cc997c06e56
kento fujimoto <kento1216mowss@gmail.com>
2020-05-24 07:25:00 +0000

    update

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ module Mypet
     # default-time-zone = 'Asia/Tokyo'
      config.active_record.default_timezone = :local
      config.assets.initialize_on_precompile = false
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
